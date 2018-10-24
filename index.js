eb2ad6c594958c4ac86a728a1c0c5d9c687a6743
Louinis BOUDAA <lboudaa@e1r8p19.42.fr>
2018-10-24 22:24:01 +0200

    new broken version

--- index.js
+++ index.js
@@ -1,80 +1,35 @@
-var http = require('http');
-var fs = require('graceful-fs');
+const request = require('request-promise')
 
-function openChest (res) {
-	res.on('data', (chunk) => {
-		if (JSON.parse(chunk.toString('utf8')).status == "It looks like there is something here!") {
-				fs.appendFile("./chests", JSON.parse(chunk.toString('utf8')).id + '\n', (err) => {
-			    	if (err) {
-			    		return console.log(err);
-				    }
-				});
+const host = 'http://castles.poulpi.fr';
+const header = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36';
+
+async function checkRoom (data) {
+	data.chests.forEach(async function (chest) {
+		try {
+			let checkChest = await request.get({uri: host + chest, headers: header, json: true});
+			if (checkChest.status == "It looks like there is something here!")
+				console.log(checkChest.id);
+		} catch(err) {
+		    return Promise.reject(new Error(400));
+		}
+	});
+	data.rooms.forEach(async function (room) {
+		try {
+			let newRoom = await request.get({uri: host + room, headers: header, json: true});
+			checkRoom(newRoom);
+		} catch(err) {
+		    return Promise.reject(new Error(400));
 		}
 	});
 }
 
-function wasNotVisited (room) {
-	fs.readFile("./visited", (err, data) => {
-    	if (err) {
-    		return console.log(err);
-	    }
-		data.toString().split('\n').forEach((line) => {
-			if (room == line) {
-				return (0);
-			}
-		});
-	});
-	return (1);
+async function main () {
+	try {
+		let data = await request.get({uri: host + '/castles/1/rooms/entry', headers: header, json: true});
+		checkRoom(data);
+	} catch(err) {
+	    return Promise.reject(new Error(400));
+	}
 }
 
-function justVisited (room) {
-	fs.appendFile("./visited", room + '\n', (err) => {
-    	if (err) {
-    		return console.log(err);
-	    }
-	});
-}
-
-function openDoor (res) {
-    var data = '';
-    var i = 0;
-    res.on('data', (chunk) => {
-        var data = JSON.parse(chunk);
-        while (data.rooms[i]) {
-        	if (wasNotVisited(data.rooms[i])) {
-	      		var request = http.request({host: 'castles.poulpi.fr', path: data.rooms[i]}, openDoor);
-	        	request.on('error', (e) => {
-	    			console.log(e.message);
-				});
-				request.end();
-	        	justVisited(data.rooms[i]);
-			}
-        	i++;
-        }
-        i = 0;
-        while (data.chests[i]) {
-        	var request = http.request({host: 'castles.poulpi.fr', path: data.chests[i]}, openChest);
-        	request.on('error', (e) => {
-    			console.log(e.message);
-			});
-			request.end();
-        	i++;
-        }
-    });
-}
-
-fs.writeFile("./visited", "", (err, data) => {
-	if (err)
-		console.log(err);
-});
-
-fs.writeFile("./chests", "", (err, data) => {
-	if (err)
-		console.log(err);
-});
-
-var request = http.request({host: 'castles.poulpi.fr', path: '/castles/1/rooms/entry'}, openDoor);
-request.on('error', (e) => {
-    console.log(e.message);
-});
-request.end();
+main();
